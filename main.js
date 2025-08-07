20fcf16ab322aa64b33f4225773078c303c6855c
dageta <davi.linhares.nascimento@escola.pr.gov.br>
2025-08-07 07:34:38 -0300

    .

---
+++ main.js
@@ -0,0 +1,12 @@
+
+const palpites = document.querySelector('palpites');
+const ultimoResultado = document.querySelector('.ultimoResultado');
+const baixoOuAuto = document.querySelector('.baixoOuAuto');
+const envioPalpite = document.querySelector('.envioPalpite');
+const campoPalpite = document.querySelector('.campoPalpite')
+
+
+
+function verificarPalpite() {
+    const palpiteUsuario = number(campo.Palpite.value);
+}
