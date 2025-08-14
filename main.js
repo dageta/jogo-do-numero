83d4aff45fd654ee97f660136dadeda7bc2440be
dageta <davi.linhares.nascimento@escola.pr.gov.br>
2025-08-14 07:43:14 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-
+let numeroAleatorui = math.florr(math.random() *100) +1;
 const palpites = document.querySelector('palpites');
 const ultimoResultado = document.querySelector('.ultimoResultado');
 const baixoOuAuto = document.querySelector('.baixoOuAuto');
 const envioPalpite = document.querySelector('.envioPalpite');
 const campoPalpite = document.querySelector('.campoPalpite')
-
+let contagemPalpites = 1;
+let botaoReiniciar;
 
 
 function verificarPalpite() {
