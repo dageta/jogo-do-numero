01093a24a0b77343afa95b6f1f71d622e11346d7
dageta <davi.linhares.nascimento@escola.pr.gov.br>
2025-08-18 10:27:43 -0300

    Update main.js

--- main.js
+++ main.js
@@ -7,10 +7,9 @@ const campoPalpite = document.querySelector('.campoPalpite')
 let contagemPalpites = 1;
 let botaoReiniciar;
 
-
 function verificarPalpite() {
-    const palpiteUsuario = number(campo.Palpite.value);
-    if (contagem.Palpites === 1) {
+    const palpiteUsuario = number(campoPalpite.value);
+    if (contagemPalpites === 1) {
         palpites.textContent = "Palpites anteriores: ";
     }
 
@@ -18,6 +17,44 @@ function verificarPalpite() {
 
     if (palpiteUsuario === neuroAleatorio) {
         ultimoResultado.textContent = "Parabens ! Você Acertou!";
-        
+        ultimoResultado.style.backgroundColor = "green";
+        baixoOuAlto.textContent = "";
+        finalizarJogo();
+    }else if (contagemPalpites === 10){
+        ultimoResultado.textcontent = "FIM DE JOGO!!";
+        baixoOuAlto.textcontent = "";
+        finalizarJogo();
+} else{
+        ultimoResultado.textContent = "Errado";
+        ultimoResultado.style.backgroundColor = "red";
+        if (palpiteUsuario < numeroAleatorio) {
+            baixoOuAlto.textContent = "O último palpite foi muito baixo";
+        } else if (palpiteUsuario > numeroAleatorio) {
+            baixoOuAlto.textContent = "o último palpite foi muito alto"
+        }
     }
+
+    contagemPalpites++;
+    campoPalpite.value = "";
+    campoPalpite.focus();
 }
+
+envioPalpite.addEventListener('click', verificarPalpite);
+
+function finalizarJogo() {
+    campoPalpite.disabled = true;
+    envioPalpite.disabled = true;
+    botaoReiniciar = document.createElement('button');
+    document.body.appendChild(botaoReiniciar);
+    botaoReiniciar.textContent = 'reiniciar jogo';
+    botaoReiniciar.classList.add('botaoReiniciar');
+    document,body.appendChild(botaoReiniciar);
+    botaoReiniciar.addEventListener('click', reiniciar jogo);
+}
+
+function reiniciarJogo() {
+    contagemPalpites = 1;
+    const paragrafosReiniciar = document.querySelectorAll('.paragrafosResultados p'} 
+for {const paragrafoReiniciar of paragrafoReiniciar) {
+    paragrafoReiniciar.textContent = "";
+        
